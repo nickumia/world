4c6efed4e02c4d87a5ab775ffc6b8cb1b7db1af6
Nicholas B Kumia <admin@ativak.io>
2021-09-26 15:20:48 -0400

    new: implement error handling for production system

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
-from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+
+from app.config import Config
+import logging
+from logging.handlers import RotatingFileHandler
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -10,4 +14,17 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 
-from app import routes, models
+from app import routes, models, errors
+
+if not app.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/nlp.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('NLP app startup')
