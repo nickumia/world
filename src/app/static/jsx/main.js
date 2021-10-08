3a41cdbfb595a50881016679cb724044d37a6e8e
nickumia <nickumia6@gmail.com>
2021-10-08 01:36:34 -0400

    new: example navbar

--- src/app/static/jsx/main.js
+++ src/app/static/jsx/main.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from '@material-ui/core/Button';
+import Navbar from './navbar';
 
 function App() {
   return <Button variant="contained">Hello World</Button>;
 }
 
 ReactDOM.render(<App />, document.querySelector('#app'));
+ReactDOM.render(<Navbar />, document.querySelector('#navbar'));
