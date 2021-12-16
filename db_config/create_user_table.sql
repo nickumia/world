b3b737360920f96a9a5ba0697058d2a32dc14af1
nickumia <nickumia6@gmail.com>
2021-12-16 04:13:13 -0500

    update: create default test user account; remove unnecessary html code

--- db_config/create_user_table.sql
+++ db_config/create_user_table.sql
@@ -5,3 +5,5 @@ CREATE TABLE IF NOT EXISTS  users (
 	password_hash		varchar(255)	UNIQUE,
 	last_seen				timestamp with time zone
 );
+
+insert into users values (100, 'asdf', 'nick@not.com', 'pbkdf2:sha256:260000$1CRXFZ2QdJM8hymf$dc06edbad9c7c009584499ee23268549402ea5b01088e98fd51e7e91999772b4');
