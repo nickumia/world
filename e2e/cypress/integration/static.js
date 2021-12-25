03cbe6b1f5f97dd3f3bab995be3daaab046a52fe
nickumia <nickumia6@gmail.com>
2021-12-25 00:14:52 -0500

    new: get cypress running :)

--- e2e/cypress/integration/static.js
+++ e2e/cypress/integration/static.js
@@ -7,7 +7,7 @@ describe('Static NLP Pages', () => {
 	});
 	
 	it('Loads the Home Page', () => {
-		cy.visit('/nlp/index');
+		cy.visit('/');
 		cy.contains('Parry');
 		cy.contains('Pleasure to be acquainted!');
 
@@ -17,3 +17,4 @@ describe('Static NLP Pages', () => {
 		cy.contains('Nick');
 		cy.contains('Welcome, I hope you find yourself at home.');
 	});
+});
--- src/app/config.py
+++ src/app/config.py
@@ -3,7 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SERVER_NAME = 'localhost:8000'
+    SERVER_NAME = os.environ['SERVER_NAME'] or 'localhost:8000'
     SECRET_KEY = os.environ['SECRET_KEY'] or 'have-fun'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
