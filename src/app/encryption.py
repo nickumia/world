a6496317ab8822267f9300f06fc676b118b6cdff
nickumia <nickumia6@gmail.com>
2021-12-26 20:45:30 -0500

    lint: phir se

--- src/app/encryption.py
+++ src/app/encryption.py
@@ -1,10 +1,10 @@
 import jinja2
-from cryptography.fernet import Fernet
+# from cryptography.fernet import Fernet
 import flask
 
 bp = flask.Blueprint('encryption', __name__)
 
-# using the decorator
+
 @jinja2.contextfilter
 @bp.app_template_filter()
 def encryptdata(context, value):
