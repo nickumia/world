5b34cb556aaca699de05cf7e9202615c84821a10
nickumia <nickumia6@gmail.com>
2021-12-26 20:36:44 -0500

    fix: defaults so that env variables don't need to be set for ci action

--- src/app/config.py
+++ src/app/config.py
@@ -1,12 +1,16 @@
+from cryptography.fernet import Fernet
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SERVER_NAME = os.environ['SERVER_NAME'] or 'localhost:8000'
-    SECRET_KEY = os.environ['SECRET_KEY'] or 'have-fun'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SERVER_NAME = os.getenv('SERVER_NAME', 'localhost:8000')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'have-fun')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL',
+        'postgresql://postgres:pass@db/postgres'))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
+    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL',
+                                       'http://localhost:9200')
     RESULTS_PER_PAGE = 10
+    ENCRYPTION_KEY = os.getenv('ENCRYPTION_KEY',
+                               'IS4KazwEAVpe4nMagfV54pxnuL9ufzsAsvQyjozvgTw=')
