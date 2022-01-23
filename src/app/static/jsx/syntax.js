c09311f2eeb1894b7d7022bfbcc0980e90573012
nickumia <nickumia6@gmail.com>
2022-01-23 10:02:17 -0500

    new: skeleton syntax app

---
+++ src/app/static/jsx/syntax.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+
+import json_parse from './json_parse';
+
+
+export default function SyntaxApp({details}) {
+
+  var page_details = json_parse(details);
+
+  return (
+    <React.Fragment>
+
+    </React.Fragment>
+  );
+}
