996d3b2d3267b08a7d99faec3dcb0b51329b55ac
nickumia <nickumia6@gmail.com>
2022-02-09 23:40:01 -0500

    new: add preliminary syntax app UI design

--- src/app/nlp/syntax.py
+++ src/app/nlp/syntax.py
@@ -11,12 +11,12 @@ opening = ("As with most of the concepts described here, syntax and grammar "
            "input method (or area of sensing) will define it's own "
            "compositional elements.")
 
-structure = ("The tiniest unit of 'Basic Text' are -->characters<--.  "
+structure = ("The tiniest unit of 'Basic Text' are __characters__.  "
              "Depending on the computer encoding, that can range from a very "
              "limited selection, such as ASCII (256 chars), to the more "
              "robust UTF-8 (or UTF-16) encoding which represents upwards of "
              "1,112,064 characters.\nThe next level of structure builds the "
-             "idea of -->words<--.  A word is any combination of characters "
+             "idea of __words__.  A word is any combination of characters "
              "that is not whitespace.  There are quite a bit of complexities "
              "that arise from this definition.  To name a few,")
 
@@ -26,35 +26,35 @@ structure2 = [
                          "(as in 'vs.' or 'cont.'), it is directly tied to "
                          "the word itself.  As a punctuation marker, it is not"
                          " actually a part of the word, but a separator to "
-                         "mark the next level of -->sentences<--.")},
-    {"key": 2, "value": ("Special characters can end up in -->words<--, such as "
+                         "mark the next level of __sentences__.")},
+    {"key": 2, "value": ("Special characters can end up in __words__, such as "
                          "(AT&T, T-Mobile, Chips Ahoy!, 5,000, 2K, ...) ")},
-    {"key": 3, "value": (" Proper names can represent one \"-->word<--\" with "
+    {"key": 3, "value": (" Proper names can represent one \"__word__\" with "
                          "spaces, such as (Supreme Court, Walt Disney, United "
                          "States, ...)\n In any case, each space-separated "
-                         "-->word<-- has it's own meaning.  'United' is valid.  "
+                         "__word__ has it's own meaning.  'United' is valid.  "
                          "'States' is valid.  The unification falls more into "
                          "the realm of semantics and pragmatics.")}
 ]
 
-structure3 = ("In any case, each space-separated -->word<-- has it's own "
+structure3 = ("In any case, each space-separated __word__ has it's own "
               "meaning.  'United' is valid.  'States' is valid.  The unifi"
               "cation falls more into the realm of semantics and pragmatics."
               "For all intents and purposes, spaces are the accepted separator"
               " for words in a text-based setting.  When considering other "
               "applications, such as data processing, commas might be a more "
               "important separator, as with CSVs.\nAs aforementioned, "
-              "-->sentences<-- are the next class of unit.  There are a few "
+              "__sentences__ are the next class of unit.  There are a few "
               "classifications that fall into here, including normal period-"
               "terminated sentences, exclamations(!) and questions(?).\n\n"
               "Beyond this, there are other constructs, namely paragraphs, "
               "essays, documents, books, ...  However, these become highly "
-              "specialized and add very little value beyond 'a group of"
-              "-->sentences<--'.  As a neat formula, text can be expressed as "
+              "specialized and add very little value beyond 'a group of "
+              "__sentences__'.  As a neat formula, text can be expressed as "
               "the following relationship:\nText = Group(Sentence(Word(Char)))"
-              "\n\twhere a -->Group<-- is a combination of 'n' -->sentences<--"
-              ",\n\tand a -->Sentence<-- is a combination of 'n' -->words<--,"
-              "\n\tand a -->Word<-- is a combination of 'n' -->chars<--\n")
+              "\n\twhere __Group__ is a combination of 'n'  __sentences__"
+              "\n\tand __Sentence__ is a combination of 'n'  __words__"
+              "\n\tand __Word__ is a combination of 'n'  __chars__\n")
 
 syntax_content = {
     "opening": opening,
--- src/app/static/jsx/syntax.js
+++ src/app/static/jsx/syntax.js
@@ -6,6 +6,18 @@ import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormLabel from '@mui/material/FormLabel';
+import Button from '@mui/material/Button';
+import TextareaAutosize from '@mui/material/TextareaAutosize';
+
+import Skeleton from '@mui/material/Skeleton';
+import cyan from "@material-ui/core/colors/cyan";
+
 import json_parse from './json_parse';
 
 
@@ -15,6 +27,34 @@ export default function SyntaxApp({details}) {
 	var opening_array = page_details.opening.split('\n');
 	var start_array = page_details.definition.start.split('\n');
 	var end_array = page_details.definition.end.split('\n');
+
+  const [value, setValue] = React.useState('');
+  const [error, setError] = React.useState(false);
+  const [helperText, setHelperText] = React.useState('Fun Fact: This is all done with simple regular expressions!');
+	const handleRadioChange = (event) => {
+    setValue(event.target.value);
+    setHelperText(' ');
+    setError(false);
+  };
+	const typo_style = {
+		whiteSpace: 'pre-wrap',
+		marginBottom: '10px',
+		marginLeft: '30px',
+		marginRight: '30px'
+	}
+	const handleSubmit = (event) => { return False;}
+	// 			    event.preventDefault();
+
+  //   if (value === 'best') {
+  //     setHelperText('You got it!');
+  //     setError(false);
+  //   } else if (value === 'worst') {
+  //     setHelperText('Sorry, wrong answer!');
+  //     setError(true);
+  //   } else {
+  //     setHelperText('Please select an option.');
+  //     setError(true);
+  //   }
 
   return (
     <React.Fragment>
@@ -27,27 +67,66 @@ export default function SyntaxApp({details}) {
 			  justifyContent="center"
 			  style={{ minHeight: '100vh' }}>
 				<Box sx={{ minWidth: 275, maxWidth: '80%' }}>
-					<Card variant="outlined">
+					<Card variant="outlined" style={{backgroundColor: cyan[50]}}>
 						<CardContent>
 							{opening_array.map(thing => (
 								<Typography
-									style={{whiteSpace: 'pre-wrap', marginBottom: '10px'}}
+									style={typo_style}
 									color="textSecondary" >{thing}</Typography>
 							))}
-							<Typography variant="h6" style={{marginBottom: '10px'}} >Structure</Typography>
-							<Typography style={{whiteSpace: 'pre-wrap', marginBottom: '10px'}} >{page_details.definition.start}</Typography>
+							<Typography variant="h6" style={{marginBottom: '10px'}} >Motivation | characters</Typography>
+							<Typography style={typo_style} >{start_array[0]}</Typography>
+							<Typography variant="h6" style={{marginBottom: '10px'}} >Motivation | words</Typography>
+							<Typography style={typo_style} >{start_array[1]}</Typography>
 							<ul>
 								{page_details.definition.middle.map(lis => (
-									<Typography style={{whiteSpace: 'pre-wrap', marginBottom: '10px' }} component="li"
+									<Typography style={typo_style} component="li"
 										key={lis.key}>{lis.value}</Typography>
 								))}
 							</ul>
+							<Typography variant="h6" style={{marginBottom: '10px'}} >Motivation | sentences and more</Typography>
 							{end_array.map(thing => (
-								<Typography style={{whiteSpace: 'pre-wrap', marginBottom: '10px'}} >{thing}</Typography>
+								<Typography style={typo_style} >{thing}</Typography>
 							))}
 						</CardContent>
 					</Card>
 				</Box>
+				<Skeleton variant="rectangular" animation="wave"
+					style={{ marginTop: '24px' }} width={'100%'} height={'16px'} />
+		    <form onSubmit={handleSubmit}>
+		      <FormControl sx={{ m: 3 }} error={error} variant="standard">
+				    <FormLabel id="syntax-radios" style={{ justifyContent: "center" }} >
+							Choose a structure class to extract
+						</FormLabel>
+						<div style={{ display: "flex" }}>
+						  <RadioGroup
+							  aria-labelledby="syntax-radios"
+					      name="options"
+						    value={value}
+							  onChange={handleRadioChange} >
+						    <FormControlLabel value="words" control={<Radio />} label="WORDS" />
+							  <FormControlLabel value="quotes" control={<Radio />} label="QUOTES" />
+								<FormControlLabel value="sentences" control={<Radio />} label="SENTENCES" />
+						    <FormControlLabel value="questions" control={<Radio />} label="QUESTIONS" />
+			          <FormControlLabel value="exclamations" control={<Radio />} label="EXCLAMATIONS" />
+					      <FormControlLabel value="enclosure" control={<Radio />} label="ENCLOSURES" />
+								<FormControlLabel value="numbers" control={<Radio />} label="NUMBERS" />
+					      <FormControlLabel value="number_expressions" control={<Radio />} label="NUMBERS WITH CONTEXT" />
+						  </RadioGroup>
+							<TextareaAutosize
+							  maxRows={10}
+							  aria-label="text input"
+							  defaultValue={page_details.definition.end}
+							  style={{ width: "60%", height: 'auto', margin: '16px', padding: '8px' }} />
+						</div>
+					  <FormHelperText>{helperText}</FormHelperText>
+						<Button sx={{ mt: 1, mr: 1 }} type="submit" variant="outlined">
+				      Evaluate 😮
+					  </Button>
+				  </FormControl>
+			  </form>
+				<Skeleton variant="rectangular" animation="wave"
+					width={'100%'} height={'16px'} />
 			</Grid>
     </React.Fragment>
   );
