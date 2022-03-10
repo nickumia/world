97aa38a9e8d5545c50db6fe437699f0a6aa7870e
nickumia <nickumia6@gmail.com>
2022-03-10 00:52:22 -0500

    fix: offline banner should be isolated from front page

--- src/app/static/jsx/offline.js
+++ src/app/static/jsx/offline.js
@@ -20,12 +20,6 @@ import json_parse from './json_parse';
 
 export default function Offline() {
 
-	const domain = {
-		'Processing': '/static/img/balloons.jpg',
-		'Language': '/static/img/mountains.jpg',
-		'Natural Core': '/static/img/sunset.jpg'
-	};
-
   return (
     <React.Fragment>
 			<Paper
@@ -43,19 +37,6 @@ export default function Offline() {
 					</Box>
 					<Box> <p style={{height: 'inherit', fontSize: 30}}> &#129402; </p> </Box>
 				</List>
-			</Paper>
-			<Paper
-				style={{backgroundColor: cyan[50], marginTop: '10px'}} sx={{px: 8, py: 4}}>
-				<Typography variant="h5">
-					Hi there!
-				</Typography>
-				<Typography variant="h6">
-					This site is dedicated to applying Natural Language Processing for a better world &#128513;
-				</Typography>
-				<Typography variant="subtitle2">
-					<br/>Thanks for taking your time to check out this website.  There are sooo many cool things in the world!  So many things that have worlds of their own.  So many things that unite or clash to form new worlds.  So many things that unknowingly coexist.  As living things, we get to interact with these things and create our own worlds from our experiences.  Please forgive me for my minor abuse of the word 'thing'.  The thing is that there's so much breadth and depth to every aspect of life that it's inefficient to be specific.  On the contrary, it's rather disrespectful to be general.  One of our daily struggles is processing a wealth of information and choosing the right level of detail for each aspect.  <br/><br/>
-					Various concepts are explored here that help to formulate the construction, destruction and transformation of our understanding of "things" &#128521;  All of the ideas presented in this journey are based in either the individual realms of 'Processing', 'Language', 'Natural Core' or (more likely) an interaction between them.  The 'Processing' realm deals with the input and output of data through our five (or more?! &#128559;) senses.  The 'Language' realm nurtures the nuances of meaning that have been formed through the establishment of cultures and societies.  The 'Natural Core' realm tears apart the manifestations of the idea to discern the abstract idea that is contained at the core of our communication.  Each of these realms are ever-evolving and, as such, this website is a work in progress.  If one considers the education system, our understanding of the universe has evolved and the knowledge that is taught in schools has been updated accordingly.  Similarly, with more research and more interactions, this website will evolve too!
-				</Typography>
 			</Paper>
 			
     </React.Fragment>
