15689ab66ad77164c2b8e9cf6946d3c775d013cd
nickumia <nickumia6@gmail.com>
2023-03-03 22:39:35 -0500

    fix: offline templates
    - Add react.css to templates
    - Update line count for substitution
    - Update nav link to remove .html
    - Remove background color

--- static.sh
+++ static.sh
@@ -3,25 +3,25 @@
 # Export processing.html
 docker run --rm -v `pwd`:/app nlp-web:debug bash -c "python3 src/utilities/tojson.py processing > testtest"
 
-cat <(sed -n '1,25p' src/app/static/offline/realm_template.html) testtest <(sed -n '26,1000p' src/app/static/offline/realm_template.html) > src/app/static/offline/processing
+cat <(sed -n '1,26p' src/app/static/offline/realm_template.html) testtest <(sed -n '27,1000p' src/app/static/offline/realm_template.html) > src/app/static/offline/processing
 rm -rf testtest
 sleep 2
 
 # Export language.html
 docker run --rm -v `pwd`:/app nlp-web:debug bash -c "python3 src/utilities/tojson.py language > testtest"
 
-cat <(sed -n '1,25p' src/app/static/offline/realm_template.html) testtest <(sed -n '26,1000p' src/app/static/offline/realm_template.html) > src/app/static/offline/language
+cat <(sed -n '1,26p' src/app/static/offline/realm_template.html) testtest <(sed -n '27,1000p' src/app/static/offline/realm_template.html) > src/app/static/offline/language
 rm -rf testtest
 sleep 2
 
 # export natural.html
 docker run --rm -v `pwd`:/app nlp-web:debug bash -c "python3 src/utilities/tojson.py natural > testtest"
 
-cat <(sed -n '1,25p' src/app/static/offline/realm_template.html) testtest <(sed -n '26,1000p' src/app/static/offline/realm_template.html) > src/app/static/offline/natural
+cat <(sed -n '1,26p' src/app/static/offline/realm_template.html) testtest <(sed -n '27,1000p' src/app/static/offline/realm_template.html) > src/app/static/offline/natural
 rm -rf testtest
 
 # export kumia.html
 docker run --rm -v `pwd`:/app nlp-web:debug bash -c "python3 src/utilities/tojson.py kumia > testtest"
 
-cat <(sed -n '1,25p' src/app/static/offline/kumia_template.html) testtest <(sed -n '26,1000p' src/app/static/offline/kumia_template.html) > src/app/static/offline/kumia
+cat <(sed -n '1,26p' src/app/static/offline/kumia_template.html) testtest <(sed -n '27,1000p' src/app/static/offline/kumia_template.html) > src/app/static/offline/kumia
 rm -rf testtest
