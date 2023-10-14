6bc27f1aa27a1bb381e2d0378fe9e8b866bdddd8
nickumia <nickumia6@gmail.com>
2023-10-14 10:35:51 -0400

    fix: everything else?
    - make install-front
    - make build-static
    - ./static_upload.sh
    - htmlsafe_dumps was removed: https://flask.palletsprojects.com/en/2.3.x/changes/#version-2-3-
    - The formal output of make-static is different, but it seems to work... so not questioning it

--- src/utilities/tojson.py
+++ src/utilities/tojson.py
@@ -1,5 +1,5 @@
 
-from flask import json as fjson
+import flask
 import json
 import sys
 
@@ -10,24 +10,24 @@ from app.kumia.resume import publications, experiences, educations
 
 # Processing page
 if sys.argv[1] == 'processing':
-    print("domain=" + fjson.htmlsafe_dumps(json.dumps(openings_parry)))
-    print("asteroids=" + fjson.htmlsafe_dumps(json.dumps(
+    print("domain=" + flask.json.dumps(json.dumps(openings_parry)))
+    print("asteroids=" + flask.json.dumps(json.dumps(
         asteroid_parry_json)))
 
 # Language page
 if sys.argv[1] == 'language':
-    print("domain=" + fjson.htmlsafe_dumps(json.dumps(openings_lalita)))
-    print("asteroids=" + fjson.htmlsafe_dumps(json.dumps(
+    print("domain=" + flask.json.dumps(json.dumps(openings_lalita)))
+    print("asteroids=" + flask.json.dumps(json.dumps(
         asteroid_lalita_json)))
 
 # Natural page
 if sys.argv[1] == 'natural':
-    print("domain=" + fjson.htmlsafe_dumps(json.dumps(openings_nick)))
-    print("asteroids=" + fjson.htmlsafe_dumps(json.dumps(
+    print("domain=" + flask.json.dumps(json.dumps(openings_nick)))
+    print("asteroids=" + flask.json.dumps(json.dumps(
         asteroid_nick_json)))
 
 # Kumia page
 if sys.argv[1] == 'kumia':
-    print("work=" + fjson.htmlsafe_dumps(json.dumps(experiences)))
-    print("pubs=" + fjson.htmlsafe_dumps(json.dumps(publications)))
-    print("edu=" + fjson.htmlsafe_dumps(json.dumps(educations)))
+    print("work=" + flask.json.dumps(json.dumps(experiences)))
+    print("pubs=" + flask.json.dumps(json.dumps(publications)))
+    print("edu=" + flask.json.dumps(json.dumps(educations)))
