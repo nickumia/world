5fb76a7d1227792aa70b795c62534e740f54e615
nickumia <nickumia6@gmail.com>
2024-03-09 14:07:21 -0500

    new: use an imagelist for main visual + button groups for options

--- static/src/jsx/financial.js
+++ static/src/jsx/financial.js
@@ -15,100 +15,63 @@ import CardActionArea from "@mui/material/CardActionArea";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import { styled } from "@mui/material/styles";
+import ButtonGroup from '@mui/material/ButtonGroup';
 import Button from "@mui/material/Button";
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
 
 import json_parse from './json_parse';
 
-const StyledCard = styled(Card)(({ theme: { breakpoints, spacing } }) => ({
-  width: 360,
-  maxWidth: "100%",
-
-  // 16px
-  borderRadius: spacing(2),
-
-  transition: "0.3s",
-  boxShadow: "0px 14px 80px rgba(34, 35, 58, 0.2)",
-  position: "relative",
-  overflow: "initial",
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  textAlign: "center",
-  paddingLeft: 8,
-  paddingRight: 8,
-
-  backgroundImage: "url(./financial_concepts.mmd.svg)",
-
-  [breakpoints.up("sm")]: {
-    textAlign: "left",
-    flexDirection: "row-reverse",
+const itemData = [
+  {
+    img: "./financial_concepts.mmd.svg",
+    title: 'Financial Literacy',
   },
-}));
-
-const CardMediaMedia = styled(CardMedia)(({ theme: { breakpoints } }) => ({
-  flexShrink: 0,
-  width: "30%",
-  paddingTop: "30%",
-  marginLeft: "auto",
-  marginRight: "auto",
-
-  [breakpoints.up("sm")]: {
-    marginRight: "initial",
-  },
-}));
-
-const TypographyOverline = styled(Typography)(({}) => ({
-  lineHeight: 2,
-  color: "#ffffff",
-  fontWeight: "bold",
-  fontSize: "0.625rem",
-  opacity: 0.7,
-}));
-
-const TypographyHeading = styled(Typography)(({}) => ({
-  fontWeight: 900,
-  color: "#ffffff",
-  letterSpacing: 0.5,
-}));
-
-const StyledButton = styled(Button)(({ theme: { breakpoints } }) => ({
-  backgroundColor: "rgba(255, 255, 255, 0.15)",
-  borderRadius: 100,
-  paddingLeft: 32,
-  paddingRight: 32,
-  color: "#ffffff",
-  textTransform: "none",
-  width: "100%",
-
-  "&:hover": {
-    backgroundColor: "rgba(255, 255, 255, 0.32)",
-  },
-
-  [breakpoints.up("sm")]: {
-    width: "auto",
-  },
-}));
+];
 
 export default function Financial() {
 
   return (
     <React.Fragment>
 
+      <ImageList sx={{ width: "100%", height: "100%" }} cols={1} rowHeight={'100%'}>
+        {itemData.map((item) => (
+          <ImageListItem key={item.img}>
+            <img
+              srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+              src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+              alt={item.title}
+              loading="lazy"
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
 
-<StyledCard
-    image={"./financial_concepts.mmd.svg"}>
-      <CardContent>
-        <TypographyOverline variant={"overline"}>
-          Songkran Festival Contest
-        </TypographyOverline>
-        <TypographyHeading variant={"h6"} gutterBottom>
-          Result Announced
-        </TypographyHeading>
-        <StyledButton>View Winners</StyledButton>
-      </CardContent>
-    </StyledCard>
+    <ButtonGroup
+      color="primary"
+      disabled={false}
+      orientation="horizontal"
+      size="large"
+      variant="outlined"
+      style={{display: "flex", justifyContent: "center", paddingTop: "2%"}}
+    >
+      <Button>Living Above<br/>Your Means</Button>
+      <Button>Living At<br/>Your Means</Button>
+      <Button>Living Below<br/>your Means</Button>
+    </ButtonGroup>
 
-    //    image={"./financial_lifestyle.mmd.svg"}
+    <ButtonGroup
+      color="primary"
+      disabled={false}
+      orientation="horizontal"
+      size="large"
+      variant="outlined"
+      style={{display: "flex", justifyContent: "center", paddingTop: "2%"}}
+    >
+      <Button>Net Worth</Button>
+      <Button>Income</Button>
+    </ButtonGroup>
+
     </React.Fragment>
   );
 }
