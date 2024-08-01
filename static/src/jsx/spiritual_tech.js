5548136c0c319dc51ee6e5ce4e34469ec7eca2ba
nickumia <nickumia6@gmail.com>
2024-08-01 18:10:38 -0400

    new: rough timeline of trip
    'ideal' conditions

--- static/src/jsx/spiritual_tech.js
+++ static/src/jsx/spiritual_tech.js
@@ -27,6 +27,14 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
+
 
 import json_parse from './json_parse';
 
@@ -117,6 +125,9 @@ export default function SpiritualTech() {
           as such, I will be integrating into communities to learn and grow with them.
 				</Typography>
     <br/>
+		<Typography variant="h4">
+			Trailers
+		</Typography>
 				<Typography variant="subtitle2">
           Check out the trailers for my trip to get a feel for what it'll be like.  If you like it,
           please reach out and let's make some great stuff happen together!
@@ -134,6 +145,9 @@ export default function SpiritualTech() {
 </div>
     <br/>
 
+		<Typography variant="h4">
+			Maps
+   	</Typography>
 				<Typography variant="subtitle2">
           There are two maps that outline (1) the points of interest that will define the trip and (2) the
           rough path that I'll take across America.  The points of interest range from state capitals to
@@ -152,6 +166,257 @@ export default function SpiritualTech() {
   <iframe class="trailer" src="https://www.google.com/maps/d/u/0/embed?mid=1oWfaNPsgA3cwiXu2pXvIBZT81r2FEnQ&ehbc=2E312F"></iframe>
   <iframe class="trailer" src="https://www.google.com/maps/d/u/0/embed?mid=1qRUtINRkfm8DJ-hUngADUpqztMpWRF0&ehbc=2E312F"></iframe>
 </div>
+		<Typography variant="h4">
+			Timeline
+   	</Typography>
+    <Timeline position="right">
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 1 | Aug 11 - Aug 17</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>Florida | Orlando, Tallahassee</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 2 | Aug 18 - Aug 24</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>Alabama | Montgomery</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 3 | Aug 25 - Aug 31</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>Louisiana | Baton Rouge, New Orleans</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 4 | Sep 01 - Sep 07</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>Mississippi</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 5 | Sep 08 - Sep 14</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>Arkansas</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 6 | Sep 15 - Sep 21</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>Oklahoma</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 7 | Sep 22 - Sep 28</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>Texas</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 8 | Sep 29 - Oct 05</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>New Mexico</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 9 | Oct 06 - Oct 12</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>Arizona</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 10 | Oct 13 - Oct 19</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>Nevada/California</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 11 | Oct 20 - Oct 26</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>Nevada/California</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 12 | Oct 27 - Nov 02</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>California Extra</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 13 | Nov 03 - Nov 09</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>Oregon</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 14 | Nov 10 - Nov 16</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>Washington</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 15 | Nov 17 - Nov 23</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>Idaho</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 16 | Nov 24 - Nov 30</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>Montana</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 17 | Dec 01 - Dec 07</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>Wyoming</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 18 | Dec 08 - Dec 14</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>North Dakota</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 19 | Dec 15 - Dec 21</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>South Dakota</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 20 | Dec 22 - Dec 28</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>Nebraska</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 21 | Dec 29 - Jan 04</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>Kansas</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 22 | Jan 05 - Jan 11</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>Missouri</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 23 | Jan 12 - Jan 18</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>Tennessee</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 24 | Jan 19 - Jan 25</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>Kentucky</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 25 | Jan 26 - Feb 01</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>Indiana</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 26 | Feb 02 - Feb 08</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>Illinois</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 27 | Feb 09 - Feb 15</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>Iowa</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 28 | Feb 16 - Feb 22</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>Minnesota</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 29 | Feb 23 - Mar 01</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>Wisconsin</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 30 | Mar 02 - Mar 08</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>Michigan</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 31 | Mar 09 - Mar 15</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>Ohio</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 32 | Mar 16 - Mar 22</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>West Virginia</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 33 | Mar 23 - Mar 29</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>Pennsylvania</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 34 | Mar 30 - Apr 05</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>New York</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 35 | Apr 06 - Apr 12</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>Vermont</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 36 | Apr 13 - Apr 19</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>Maine</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 37 | Apr 20 - Apr 26</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>New Hampshire</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 38 | Apr 27 - May 03</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>Massachusetts</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 39 | May 04 - May 10</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>Rhode Island</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 40 | May 11 - May 17</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>Connecticutt</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 41 | May 18 - May 24</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>New York</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 42 | May 25 - May 31</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>New Jersey</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 43 | Jun 01 - Jun 07</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>Delaware</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 44 | Jun 08 - Jun 14</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>Maryland</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 45 | Jun 15 - Jun 21</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>Virginia</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 46 | Jun 22 - Jun 28</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>North Carolina</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 47 | Jun 29 - Jul 05</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>South Carolina</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 48 | Jul 06 - Jul 12</TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>Georgia</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary"> Week 49 | Jul 13 - </TimelineOppositeContent>
+        <TimelineSeparator> <TimelineDot /> <TimelineConnector /> </TimelineSeparator>
+        <TimelineContent>Florida</TimelineContent>
+      </TimelineItem>
+    </Timeline>
+    <br/>
 
 <br style={{clear: "both"}}/>
 		<Typography variant="h4">
