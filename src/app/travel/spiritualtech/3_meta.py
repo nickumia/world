391c17b5f0eb5eda5acf31765e2e289282cf13a6
nickumia <nickumia6@gmail.com>
2025-09-05 00:56:06 -0400

    new: day 3 thoughts

---
+++ src/app/travel/spiritualtech/3_meta.py
@@ -0,0 +1,3 @@
+# Metadata for the SpiritualTech travel post
+title = "One Year Later: Day 3"
+posted_time = "August 31, 2025"
--- static.sh
+++ static.sh
@@ -12,6 +12,7 @@ declare -a simple_pages=(
 
 declare -a content_pages=(
 # [0]='d20141228 A_Beginning_20141228 singlepost'
+[997]='spiritualtech/3 Day_3 singlepost'
 [998]='spiritualtech/2 Day_2 singlepost'
 [999]='spiritualtech/1 Day_1 singlepost'
 [1000]='spiritualtech/one_year_later SpiritualTech_One_Year_Later singlepost'
