243d857f3fa89871bcc5282e2702b9b1d222ae3a
nickumia <nickumia6@gmail.com>
2025-10-12 01:21:43 -0400

    new: understanding learning vs. knowledge post

---
+++ src/app/nlp/posts/learning_v_knowledge_meta.py
@@ -0,0 +1,5 @@
+# Metadata for Learning v Knowledge post
+title = "Knowledge is Not a File: Redefining What it Means to 'Know'"
+subtitle = "Connections and Coherence: Knowledge as a Dynamic Field of Understanding"
+posted_time = "October 11, 2025"
+author = "nick"
