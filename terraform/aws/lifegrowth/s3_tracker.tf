495ef3e11f973b2e4ec42fb8fddfbfa6d0fb5cc2
nickumia <nickumia6@gmail.com>
2026-02-16 18:58:29 -0500

    fix: remove duplicate entrypoint

--- terraform/aws/lifegrowth/s3_tracker.tf
+++ terraform/aws/lifegrowth/s3_tracker.tf
@@ -125,23 +125,6 @@ resource "aws_api_gateway_integration" "life_tracker_integration" {
   uri                     = aws_lambda_function.life_tracker_lambda.invoke_arn
 }
 
-resource "aws_api_gateway_method" "proxy_method" {
-  rest_api_id   = aws_api_gateway_rest_api.life_tracker_api.id
-  resource_id   = aws_api_gateway_resource.life_tracker_resource.id
-  http_method   = "ANY"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "proxy_integration" {
-  rest_api_id = aws_api_gateway_rest_api.life_tracker_api.id
-  resource_id = aws_api_gateway_resource.life_tracker_resource.id
-  http_method = aws_api_gateway_method.proxy_method.http_method
-
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.life_tracker_lambda.invoke_arn
-}
-
 resource "aws_api_gateway_deployment" "life_tracker_deployment" {
   rest_api_id = aws_api_gateway_rest_api.life_tracker_api.id
 
