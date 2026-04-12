0f7d8baadbc27d86ee2643b051c991c8246feff3
nickumia <nickumia6@gmail.com>
2026-04-12 17:34:36 -0400

    fix: more... missing emoji in titles

--- src/app/kumia/inhibition_meta.py
+++ src/app/kumia/inhibition_meta.py
@@ -1,3 +1,3 @@
 # Metadata for the keep_notes post
-title = "How do Biological and Sociological Inhibition affect Behaviors?"
+title = "How do Biological and Sociological Inhibition affect Behaviors? 🧬"
 posted_time = "September 27th, 2025"
